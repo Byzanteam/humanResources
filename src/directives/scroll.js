a2b7e0b729c5afc93731f0c39e64343a932d4aa4
idcjaja <838927992@qq.com>
2020-11-13 17:12:33 +0800

    fix: scroll reset

--- src/directives/scroll.js
+++ src/directives/scroll.js
@@ -1,58 +1,65 @@
-var timr = null
-var limit = 0
-var distance = 0
-var duration = '1.2s'
-
-const init = el => {
-  if (timr) {
-    clearInterval(timr)
+class Scroll {
+  constructor({el, value: {itemHeight}}) {
+    this.el = el
+    this.itemHeight = itemHeight
+    this.timr = null
+    this.limit = 0
+    this.distance = 0
+    this.duration = '1.2s'
+    this.itemHeight = itemHeight
+    this.scrollAuto()
   }
 
-  scrollTop(el)
-}
-
-const setStyle = el => {
-  el.style.transitionDuration = duration
-  el.style.transitionProperty = 'transform'
-  el.style.transform = `translate3d(0px, ${distance}px, 0px)`
-}
-
-const scrollTop = el => {
-  distance = 0
-  duration = '0s'
-  setStyle(el)
-}
-
-const scrollAuto = (el, { itemHeight }) => {
-  init(el)
-  const clientHeight = el.parentNode.clientHeight,
-        scrollHeight = el.parentNode.scrollHeight
-  if (scrollHeight <= clientHeight) return
-  limit = scrollHeight
-  setTimr(el, itemHeight)
-}
-
-const setTimr = (el, itemHeight) => {
-  timr = setInterval(() => {
-    if(distance >= limit) {
-      scrollTop(el)
-    } else {
-      if(duration === '0s') {
-        duration = '1.2s'
-        setStyle(el)
-      }
-      distance += itemHeight
-      el.style.transform = `translate3d(0px, -${distance}px, 0px)`
+  init () {
+    if (this.timr) {
+      this.clearInterval(this.timr)
     }
+    this.scrollTop()
+  }
 
-  }, 5000);
+  setStyle () {
+    this.el.style.transitionDuration = this.duration
+    this.el.style.transitionProperty = 'transform'
+    this.el.style.transform = `translate3d(0px, -${this.distance}px, 0px)`
+  }
+
+  scrollTop () {
+    this.distance = 0
+    this.duration = '0s'
+    this.setStyle()
+  }
+
+  scrollAuto () {
+    this.init()
+    const clientHeight = this.el.parentNode.clientHeight,
+          scrollHeight = this.el.parentNode.scrollHeight
+    if (scrollHeight <= clientHeight) return
+    this.limit = scrollHeight
+    this.setTimr()
+  }
+
+  setTimr () {
+    clearInterval(this.timr)
+    this.timr = setInterval(() => {
+      if(this.distance >= this.limit) {
+        this.scrollTop(this.el)
+      } else {
+        if(this.duration === '0s') {
+          this.duration = '1.2s'
+        }
+        this.distance += this.itemHeight
+        this.setStyle()
+      }
+    }, 5000);
+  }
 }
 
 export default {
   update(el, { value }) {
-    scrollAuto(el, value)
+    if(el._scroll) clearInterval(el._scroll.timr)
+    el._scroll = new Scroll({el, value})
   },
-  unbind() {
-    clearInterval(timr)
+  unbind(el) {
+    clearInterval(el._scroll.timr)
   }
 }
