77988054f4dd6f23bd6a791988a8652d43d17a28
ll790226724 <790226724@qq.com>
2021-07-29 14:35:55 +0800

    替换筛选条件

--- public/lib/env.js
+++ public/lib/env.js
@@ -1,6 +1,6 @@
 //后端服务地址
 window.BACKEND_HOST = 'http://service.testbuild.youedata.cc/ftest/ftest-server';
 
-window.appRequestId = '9f5c2cc6-f4cc-4757-8a01-7d79cbb06125';
-window.apiID = '9a7c1d5e-2380-49ab-940b-56b70fc69b3e';
-window.apiKey = '54fc233d9f2b4aa3a7f7b3bf04f4d158';
+window.appRequestId = '8a5378f2-ed13-444c-a10c-b0dac590b0db';
+window.apiID = '5f9b06b4-5314-4228-a5c3-73bf8c684be9';
+window.apiKey = 'd5f67b567a894b9eac01813073f104ba';
